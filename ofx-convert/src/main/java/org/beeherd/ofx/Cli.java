7cc473cd651af61ec0711a9c4a85479d08266507
Samuel Cox <crankydillo@gmail.com>
2019-08-31 09:25:46 -0500

    OFX utility for converting from OFXv2 to OFXv1
    Made so that libofx+gnucash will work with amex.

---
+++ ofx-convert/src/main/java/org/beeherd/ofx/Cli.java
@@ -0,0 +1,30 @@
+package org.beeherd.ofx;
+
+import java.io.*;
+
+import com.webcohesion.ofx4j.domain.data.ResponseEnvelope;
+import com.webcohesion.ofx4j.io.AggregateMarshaller;
+import com.webcohesion.ofx4j.io.AggregateUnmarshaller;
+import com.webcohesion.ofx4j.io.v1.OFXV1Writer;
+
+public class Cli {
+
+  public static void main(final String[] args) throws Exception {
+    final File file = new File(args[0]);
+    final InputStream in = new FileInputStream(file);
+    final OFXV1Writer writer = new OFXV1Writer(System.out);
+
+    try {
+      final AggregateUnmarshaller<ResponseEnvelope> unmarshaller =
+              new AggregateUnmarshaller<>(ResponseEnvelope.class);
+      final Object data = unmarshaller.unmarshal(in);
+      new AggregateMarshaller().marshal(data, writer);
+      writer.flush();
+    } finally {
+      try { in.close(); } catch (Exception e) {}
+      try { writer.close(); } catch (Exception e) {}
+    }
+
+  }
+
+}
